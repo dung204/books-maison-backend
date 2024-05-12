3b1653e69c7299e58f8b8b8c8f56d4d044ededa1
dung204 <acezombiev4@gmail.com>
2024-05-12 23:16:45 +0700

    feat: swagger schema for pagination query

--- src/base/common/dto/pagination-query.dto.ts
+++ src/base/common/dto/pagination-query.dto.ts
@@ -1,13 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsInt, IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationQueryDto {
+  @ApiProperty({
+    description: 'The current page number',
+    example: 1,
+    required: false,
+  })
   @Transform(({ value }) => parseInt(value, 10))
   @IsInt({ message: 'Page must be an integer' })
   @IsPositive({ message: 'Page must be a positive integer' })
   @IsOptional()
   page?: number = 1;
 
+  @ApiProperty({
+    description: 'The number of items in a page',
+    example: 10,
+    required: false,
+  })
   @Transform(({ value }) => parseInt(value, 10))
   @IsInt({ message: 'Page size must be an integer' })
   @IsPositive({ message: 'Page size must be a positive integer' })
