02c61791feb6793d9860dd6dd029789a9f6c1b92
dung204 <acezombiev4@gmail.com>
2024-05-24 10:39:01 +0700

    docs: implement swagger in auth routes

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -7,32 +7,85 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { CustomRequest } from '@/base/common/types/custom-request.type';
 import { JwtAccessGuard } from '@/modules/auth/guards/jwt-access.guard';
 import { LocalAuthGuard } from '@/modules/auth/guards/local-auth.guard';
+import { LoginRequest } from '@/modules/auth/requests/login.request';
+import { RefreshRequest } from '@/modules/auth/requests/refresh.request';
+import { LoginSuccessResponse } from '@/modules/auth/responses/login-success.response';
 import { AuthService } from '@/modules/auth/services/auth.service';
 
+@ApiTags('auth')
 @Controller('/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Login to the system' })
+  @ApiBody({
+    type: LoginRequest,
+  })
+  @ApiOkResponse({
+    description: 'Successful login',
+    type: LoginSuccessResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'The email or password is invalid.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error.',
+  })
   @UseGuards(LocalAuthGuard)
   @Post('/login')
+  @HttpCode(HttpStatus.OK)
   async login(@Request() req: CustomRequest) {
     return this.authService.login(req.user);
   }
 
+  @ApiOperation({ summary: 'Create new (refresh) tokens' })
+  @ApiBody({
+    type: RefreshRequest,
+  })
+  @ApiOkResponse({ description: 'New tokens returned' })
+  @ApiUnauthorizedResponse({
+    description:
+      'Refresh token does not belong to the current user or the user did not log in',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error.',
+  })
   @UseGuards(JwtAccessGuard)
   @Post('/refresh')
+  @HttpCode(HttpStatus.OK)
   async refresh(
     @Request() req: CustomRequest,
-    @Body('refreshToken') refreshToken: string,
+    @Body() { refreshToken }: RefreshRequest,
   ) {
     const accessToken = req.headers.authorization.replaceAll('Bearer ', '');
     return this.authService.refresh(req.user, accessToken, refreshToken);
   }
 
+  @ApiOperation({
+    summary: 'Logout to the system',
+  })
+  @ApiNoContentResponse({
+    description: 'Successful logout',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'The user did not log in',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
   @UseGuards(JwtAccessGuard)
   @Post('/logout')
   @HttpCode(HttpStatus.NO_CONTENT)
