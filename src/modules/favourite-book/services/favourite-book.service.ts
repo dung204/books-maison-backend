a3a18a85131e98f6c0949c5af373375c900d02a2
dung204 <acezombiev4@gmail.com>
2024-06-24 12:26:51 +0700

    feat: add a route to add a favourite book

--- src/modules/favourite-book/services/favourite-book.service.ts
+++ src/modules/favourite-book/services/favourite-book.service.ts
@@ -1,6 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+
+import { BookService } from '@/modules/book/services/book.service';
+import { FavouriteBook } from '@/modules/favourite-book/entities/favourite-book.entity';
+import { FavouriteBookRepository } from '@/modules/favourite-book/repositories/favourite-book.repository';
+import { User } from '@/modules/user/entities/user.entity';
 
 @Injectable()
 export class FavouriteBookService {
-  constructor() {}
+  constructor(
+    @Inject(FavouriteBookRepository)
+    private readonly favouriteBookRepository: FavouriteBookRepository,
+    private readonly bookService: BookService,
+  ) {}
+
+  async addFavouriteBook(user: User, bookId: string) {
+    const hasExistedByUserIdAndBookId =
+      await this.favouriteBookRepository.isExistedByUserIdAndBookId(
+        user.id,
+        bookId,
+      );
+
+    if (hasExistedByUserIdAndBookId) {
+      throw new ConflictException(
+        'User has already added this book as favourite book.',
+      );
+    }
+
+    const book = await this.bookService.findOne(bookId);
+    const favouriteBook = new FavouriteBook();
+
+    favouriteBook.userId = user.id;
+    favouriteBook.bookId = book.id;
+
+    favouriteBook.user = user;
+    favouriteBook.book = book;
+
+    await this.favouriteBookRepository.save(favouriteBook);
+  }
 }
