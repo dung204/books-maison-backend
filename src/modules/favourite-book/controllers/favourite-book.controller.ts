09dc793beaa3f338a2d5da1cbf39b319b6627f38
dung204 <acezombiev4@gmail.com>
2024-06-24 22:31:04 +0700

    feat: add a route to get all favourite books of current user

--- src/modules/favourite-book/controllers/favourite-book.controller.ts
+++ src/modules/favourite-book/controllers/favourite-book.controller.ts
@@ -1,9 +1,11 @@
 import {
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
   Post,
+  Query,
   Request,
   UseGuards,
 } from '@nestjs/common';
@@ -18,8 +20,11 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 
+import { ApiSuccessResponse } from '@/base/common/decorators/api-success-response.decorator';
 import { CustomRequest } from '@/base/common/types/custom-request.type';
 import { JwtAccessGuard } from '@/modules/auth/guards/jwt-access.guard';
+import { Book } from '@/modules/book/entities/book.entity';
+import { FavouriteBookSearchDto } from '@/modules/favourite-book/dto/favourite-book-search.dto';
 import { FavouriteBookService } from '@/modules/favourite-book/services/favourite-book.service';
 
 @ApiBearerAuth('JWT')
@@ -54,5 +59,36 @@ export class FavouriteBookController {
   addFavouriteBook(@Request() req: CustomRequest, @Param('id') bookId: string) {
     const currentUser = req.user;
     return this.favouriteBookService.addFavouriteBook(currentUser, bookId);
+  }
+
+  @ApiOperation({
+    summary: 'Get all favourite books',
+    description: 'Get all favourite books of the current authenticated user',
+  })
+  @ApiSuccessResponse({
+    status: HttpStatus.OK,
+    schema: Book,
+    isArray: true,
+    pagination: true,
+    description:
+      'Get all favourite books information successfully (with pagination metadata)',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'User has not signed in.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error.',
+  })
+  @UseGuards(JwtAccessGuard)
+  @Get('/all')
+  getAllFavouriteBooks(
+    @Request() req: CustomRequest,
+    @Query() favouriteBookSearchDto: FavouriteBookSearchDto,
+  ) {
+    const currentUser = req.user;
+    return this.favouriteBookService.getAllFavouriteBooks(
+      currentUser,
+      favouriteBookSearchDto,
+    );
   }
 }
