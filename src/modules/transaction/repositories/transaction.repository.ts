5755cbfffddeec2086e3aa5034f6a501fe2bf05e
dung204 <acezombiev4@gmail.com>
2024-07-09 19:04:58 +0700

    feat: add a route to get all transactions

--- src/modules/transaction/repositories/transaction.repository.ts
+++ src/modules/transaction/repositories/transaction.repository.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 
+import { PaginationQueryDto } from '@/base/common/dto/pagination-query.dto';
 import { Transaction } from '@/modules/transaction/entities/transaction.entity';
 
 @Injectable()
 export class TransactionRepository extends Repository<Transaction> {
   constructor(private readonly dataSource: DataSource) {
-    super(
-      Transaction,
-      dataSource.createEntityManager(),
-      dataSource.createQueryRunner(),
-    );
+    super(Transaction, dataSource.createEntityManager());
+  }
+
+  async findAllAndCount({ page, pageSize }: PaginationQueryDto) {
+    const skip = (page - 1) * pageSize;
+    return this.createQueryBuilder('transaction')
+      .leftJoinAndSelect('transaction.user', 'user')
+      .skip(skip)
+      .take(pageSize)
+      .getManyAndCount();
   }
 }
