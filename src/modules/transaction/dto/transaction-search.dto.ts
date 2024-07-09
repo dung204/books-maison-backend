91c750f691ff8788c3372a9e63c2e00dcced3508
dung204 <acezombiev4@gmail.com>
2024-07-09 21:12:54 +0700

    feat: implement searching by methods for transactions

--- src/modules/transaction/dto/transaction-search.dto.ts
+++ src/modules/transaction/dto/transaction-search.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
 
 import { PaginationQueryDto } from '@/base/common/dto/pagination-query.dto';
+import { TransactionMethod } from '@/modules/transaction/enums/transaction-method.enum';
 
 export class TransactionSearchDto extends PaginationQueryDto {
   @ApiProperty({
@@ -11,4 +13,23 @@ export class TransactionSearchDto extends PaginationQueryDto {
   @IsOptional()
   @IsUUID(4, { message: 'User ID must be a valid UUID v4' })
   userId?: string;
+
+  @ApiProperty({
+    description:
+      'Every transactions using one of these methods will be returned',
+    enum: TransactionMethod,
+    enumName: 'TransactionMethod',
+    isArray: true,
+    required: false,
+  })
+  @IsOptional()
+  @IsEnum(TransactionMethod, {
+    each: true,
+    message: `Transaction method must be one of these values: ${Object.values(TransactionMethod).join(', ')}`,
+  })
+  @Transform(({ value }) => {
+    if (typeof value === 'string') return [value];
+    return value;
+  })
+  method?: TransactionMethod[];
 }
