bc2b206615f944c534efdace4b02e1fa0ea8036e
dung204 <acezombiev4@gmail.com>
2024-07-14 14:14:15 +0700

    fix: missing redirectUrl in PayFineDto

--- src/modules/fine/dto/pay-fine.dto.ts
+++ src/modules/fine/dto/pay-fine.dto.ts
@@ -2,4 +2,7 @@ import { PickType } from '@nestjs/swagger';
 
 import { CreateTransactionDto } from '@/modules/transaction/dto/create-transaction.dto';
 
-export class PayFineDto extends PickType(CreateTransactionDto, ['method']) {}
+export class PayFineDto extends PickType(CreateTransactionDto, [
+  'method',
+  'redirectUrl',
+]) {}
