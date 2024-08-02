3d14cd382863f2fbed73733b0ce1f222258b076b
dung204 <acezombiev4@gmail.com>
2024-08-02 13:57:53 +0700

    refactor: update checkout status from RENTING to BORROWING

---
+++ src/base/database/migrations/1722581637910-CheckoutStatusRentingToBorrowing.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CheckoutStatusRentingToBorrowing1722581637910
+  implements MigrationInterface
+{
+  name = 'CheckoutStatusRentingToBorrowing1722581637910';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TYPE "public"."checkouts_status_enum" RENAME TO "checkouts_status_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."checkouts_status_enum" AS ENUM('BORROWING', 'OVERDUE', 'RETURNED')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" TYPE "public"."checkouts_status_enum" USING "status"::"text"::"public"."checkouts_status_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" SET DEFAULT 'BORROWING'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."checkouts_status_enum_old"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."checkouts_status_enum_old" AS ENUM('RENTING', 'OVERDUE', 'RETURNED')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" TYPE "public"."checkouts_status_enum_old" USING "status"::"text"::"public"."checkouts_status_enum_old"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "checkouts" ALTER COLUMN "status" SET DEFAULT 'RENTING'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."checkouts_status_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."checkouts_status_enum_old" RENAME TO "checkouts_status_enum"`,
+    );
+  }
+}
